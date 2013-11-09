c380f044db33410d16e9da0634f49a9afe2f95cf
rysquash <rysquash@gmail.com>
2013-11-09 15:10:39 -0500

    yo

--- game.py
+++ game.py
@@ -11,10 +11,41 @@ frame.pack()
 canvas=Canvas(frame,width=500,height=500)
 canvas.pack()
 
+
+objects=[]
+
+
+class ball:
+	def __init__():
+		objects.append(self)
+	def update():
+
+		print canvas.create_oval(10,10,100,100,fill="blue")
+
+
 def restart():
 	print 'restarting'
 
+	print canvas.create_oval(10,10,100,100,fill="blue")
+
+
+def update():
+	root.after(10,update)
+
+
+
+
+
+
+
 
 Button(frame, text="Restart",command=restart).pack()
+
+root.after(10,update)
+
+restart()
+
+root.bind("<Escape>", exit)
+
 
 root.mainloop()
