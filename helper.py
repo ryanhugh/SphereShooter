58b5567f34b0c3836db612135cdb27ed43d5dc93
Arun Drelich <ad2476@gmail.com>
2013-11-09 17:24:57 -0500

    Added speed debug info

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+from math import *
 #updated by mainfile
 objects=[]
 canvas=None
@@ -14,14 +15,12 @@ class Player:
 		self.id=canvas.create_rectangle(10,10,100,100,fill="blue")
 
 	def update(self):
-		if (canvas.coords(self.id)[0] >= 400) or (canvas.coords(self.id)[0] <= 0):
+		if (canvas.coords(self.id)[2] >= 500) or (canvas.coords(self.id)[0] <= 0):
 			self.moveDir[0]*=-1
-		if (canvas.coords(self.id)[1] >= 400) or (canvas.coords(self.id)[1] <= 0):
+		if (canvas.coords(self.id)[3] >= 500) or (canvas.coords(self.id)[1] <= 0):
 			self.moveDir[1]*=-1
 			
 		canvas.move(self.id,self.moveDir[0], self.moveDir[1])
-		
-
 
 def init():
 	global player
@@ -39,7 +38,7 @@ def onclick(event):
 	if event.keysym == "b":
 		print "DEBUG DATA:"
 		print "Player coords: ", canvas.coords(player.id)
-		print "Player move vector: ", player.moveDir
+		print "Player move vector: ", player.moveDir, " speed=", sqrt(player.moveDir[0]**2+player.moveDir[1]**2)
 	elif event.keysym in ["w","a","s","d"]:
 		newMovedir={"w":[0,-1],"a":[-1,0],"s":[0,1],"d":[1,0]}[event.keysym]
 		player.moveDir[0]+=newMovedir[0]
